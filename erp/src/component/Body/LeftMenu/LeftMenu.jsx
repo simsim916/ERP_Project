0c5f6f68a8064ff1eda3da72d54b81682b2936d6
N2406009 <simsim916@naver.com>
2024-07-08 09:05:50 +0900

    24-07-08

--- erp/src/component/Body/LeftMenu/LeftMenu.jsx
+++ erp/src/component/Body/LeftMenu/LeftMenu.jsx
@@ -6,7 +6,12 @@ const LeftMenu = () =>{
 
     return (
         <div id="LeftMenu">
+            <div id="LeftMenu_Tab">
+                <div>마이메뉴</div>
+                <div>전체메뉴</div>
 
+            </div>
+            <div id="LeftMenu_Content"></div>
         </div>
     );
 }
